2bae9210e35d4c92fe2fa1353ab6316ade48f620
VikramNair <vikramnairj@gmail.com>
2016-04-09 06:21:21 -0400

    Room changes reflected to the front

--- src/apiserver/apiserver.go
+++ src/apiserver/apiserver.go
@@ -104,7 +104,7 @@ func GetServer() *APIServer {
 		//Getting the room json and calling the update
 		print("Calling the room info update method!!")
 		request := urllib.Post("http://127.0.0.1:8000/add_base_room/")
-		var roomjson = map[string]interface{}{"room_id": 1, "curr_players": "1,2", "global_comm_topic_name": "coms", "global_notification_topic_name": "notifications",
+		var roomjson = map[string]interface{}{"room_id": 1, "curr_players": "127.0.0.1,127.0.0.2", "global_comm_topic_name": "coms", "global_notification_topic_name": "notifications",
 			"no_of_policies_passed": 0, "fascist_policies_passed": 0, "liberal_policies_passed": 0, "current_fascist_in_deck": 11, "current_liberal_in_deck": 6, "current_total_in_deck": 17,
 			"chancellor_id": -1, "president_id": -1, "president_channel": "pres", "chancellor_channel": "chan", "hitler_id": -1}
 
@@ -112,6 +112,7 @@ func GetServer() *APIServer {
 		if err != nil {
 		}
 		request.String()
+
 		r.JSON(http.StatusOK, userjson)
 	})
 
@@ -126,9 +127,9 @@ func GetServer() *APIServer {
 			}
 		}
 		print("User id queries - " + userid)
-		//Get the user details from gavins method and return to front end
-		var userjson = map[string]interface{}{"user_id": "127.0.0.1", "name": "test_user", "user_type": "liberal", "node_type": "test_role", "secret_role": "hitler"}
 
+		//Get the user details from gavins method and return to front end
+		var userjson = map[string]interface{}{"user_id": userid, "name": "test_user", "user_type": "liberal", "node_type": "test_role", "secret_role": "hitler"}
 		r.JSON(http.StatusOK, userjson)
 	})
 
