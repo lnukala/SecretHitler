d53166cde159bcaf2482c002ec37c48f75d4a8d6
GavinCode <gmy.gavin@gmail.com>
2016-04-08 18:00:02 -0700

    renaming files

---
+++ src/apiserver/apiserver.go
@@ -0,0 +1,112 @@
+package apiserver
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+// APIServer : server struct
+type APIServer struct {
+	m          *martini.ClassicMartini
+	state      int
+	super      bool
+	superList  []string
+	userList   []string
+	attachedTo string
+	uid        string
+}
+
+var singleServer *APIServer
+
+// RunServer : start the server
+func (s *APIServer) RunServer() {
+	s.m.Run()
+}
+
+// SetSuper : set the node to be supernode or subnode
+func (s *APIServer) SetSuper(isSuper bool) {
+	s.super = isSuper
+}
+
+// SetUID  : set user id
+func (s *APIServer) SetUID(userid string) {
+	s.uid = userid
+}
+
+// AddSuperNode   add one super node to the list
+func (s *APIServer) AddSuperNode(superip string) {
+	s.superList = append(s.superList, superip)
+}
+
+// AttachTo   add one super node to the list
+func (s *APIServer) AttachTo(superip string) {
+	s.attachedTo = superip
+}
+
+// AddUser   add a user
+func (s *APIServer) AddUser(uid string) {
+	s.userList = append(s.userList, uid)
+}
+
+// NotifyGet  use this method to send notification to front end. Paramemter in param map
+// @hook the url
+// @param parameters
+func NotifyGet(hook string, param map[string]string) {
+	front := "http://localhost:8000/"
+	url := front + hook + "?"
+	for k, v := range param {
+		url += k + "=" + v + "&"
+	}
+	http.Get(url)
+}
+
+// GetServer : return a http server
+func GetServer() *APIServer {
+	if singleServer != nil {
+		return singleServer
+	}
+
+	singleServer = &APIServer{state: 0, m: martini.Classic()} // initialize a server
+	singleServer.m.Use(render.Renderer())
+
+	// heartbeet  returns StatusOK with a string
+	singleServer.m.Get("/heartbeat", func() string {
+		return "I'm alive"
+	})
+
+	// login  return json data including the succes information
+	singleServer.m.Get("/login", func(args martini.Params, r render.Render) {
+		r.JSON(http.StatusOK, map[string]interface{}{"uid": -1, "is_super": singleServer.super})
+	})
+
+	// rooms  list all the rooms
+	singleServer.m.Get("/rooms", func(args martini.Params) string {
+		res, _ := json.Marshal([]int{1, 2, 3, 4})
+		return string(res)
+	})
+
+	// login  return json data including the succes information
+	singleServer.m.Get("/myrole", func(args martini.Params, r render.Render) {
+		r.JSON(http.StatusOK, map[string]interface{}{"role": "chancellor"})
+	})
+
+	// check if it's super node and see who it attach to
+	singleServer.m.Get("/issuper", func(args martini.Params, r render.Render) {
+		r.JSON(http.StatusOK, map[string]interface{}{"super": singleServer.super, "attach_to": singleServer.attachedTo})
+	})
+
+	// superlist  see the super node list
+	singleServer.m.Get("/superlist", func(args martini.Params, r render.Render) {
+		r.JSON(http.StatusOK, map[string]interface{}{"superlist": singleServer.superList})
+	})
+
+	// userlist  see the userlist
+	singleServer.m.Get("/userlist", func(args martini.Params, r render.Render) {
+		r.JSON(http.StatusOK, map[string]interface{}{"userlist": singleServer.userList})
+	})
+
+	return singleServer
+}
